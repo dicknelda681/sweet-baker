f9d3d1444afeae7b3bce55da4819afa7a7e3b0a3
dicknelda681 <dicknelda681@gmail.com>
2026-01-15 10:53:41 +0530

    d

---
+++ header.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html class="wide wow-animation ios iphone mobile portrait rd-navbar-static-linked" lang="en">
+   <head>
+      <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+      <title>Sweet Baker – Fresh Cakes, Custom Desserts & Bakery for Every Occasion</title>
+      <meta name="viewport" content="width=device-width, height=device-height, initial-scale=1.0">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <link rel="stylesheet" type="text/css" href="./assets/css2.css">
+      <link rel="stylesheet" href="./assets/all.min.css">
+      <link rel="stylesheet" href="./assets/bootstrap.css">
+      <link rel="stylesheet" href="./assets/fonts.css">
+      <link rel="stylesheet" href="./assets/style.css">
+   </head>
+   <body>
+    <!--Start of Tawk.to Script-->
+<script type="text/javascript">
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/6967956886e2fa197e0fe51d/1jeu9v8i4';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+</script>
+<!--End of Tawk.to Script-->
