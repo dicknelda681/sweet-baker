4f5741125354e43ebd08a2048a134e1ebf0c8a12
dicknelda681 <dicknelda681@gmail.com>
2026-01-15 10:57:37 +0530

    r

---
+++ footer.php
@@ -0,0 +1,90 @@
+    <footer class="section footer-modern">
+            <div class="footer-modern-body section-xl">
+               <div class="container">
+                  <div class="row row-40 row-md-50 justify-content-xl-between">
+                     <div class="col-md-10 col-lg-3 col-xl-4 wow fadeInRight">
+                        <div class="inset-xl-right-70">
+                           <h5 class="footer-modern-title">Gallery</h5>
+                           <div class="row row-10 gutters-10" data-lightgallery="group">
+                              <div class="col-4 col-sm-2 col-lg-4">
+                                 <!-- Thumbnail Minimal--><a class="thumbnail-minimal" href="images/grid-gallery-1-original.jpg" data-lightgallery="item"><img src="./assets/grid-gallery-1-130x130.jpg" alt="" width="130" height="130"></a>
+                              </div>
+                              <div class="col-4 col-sm-2 col-lg-4">
+                                 <!-- Thumbnail Minimal--><a class="thumbnail-minimal" href="images/grid-gallery-2-original.jpg" data-lightgallery="item"><img src="./assets/grid-gallery-2-130x130.jpg" alt="" width="130" height="130"></a>
+                              </div>
+                              <div class="col-4 col-sm-2 col-lg-4">
+                                 <!-- Thumbnail Minimal--><a class="thumbnail-minimal" href="images/grid-gallery-3-original.jpg" data-lightgallery="item"><img src="./assets/grid-gallery-3-130x130.jpg" alt="" width="130" height="130"></a>
+                              </div>
+                              <div class="col-4 col-sm-2 col-lg-4">
+                                 <!-- Thumbnail Minimal--><a class="thumbnail-minimal" href="images/grid-gallery-4-original.jpg" data-lightgallery="item"><img src="./assets/grid-gallery-4-130x130.jpg" alt="" width="130" height="130"></a>
+                              </div>
+                              <div class="col-4 col-sm-2 col-lg-4">
+                                 <!-- Thumbnail Minimal--><a class="thumbnail-minimal" href="images/grid-gallery-5-original.jpg" data-lightgallery="item"><img src="./assets/grid-gallery-5-130x130.jpg" alt="" width="130" height="130"></a>
+                              </div>
+                              <div class="col-4 col-sm-2 col-lg-4">
+                                 <!-- Thumbnail Minimal--><a class="thumbnail-minimal" href="images/grid-gallery-6-original.jpg" data-lightgallery="item"><img src="./assets/grid-gallery-6-130x130.jpg" alt="" width="130" height="130"></a>
+                              </div>
+                           </div>
+                        </div>
+                     </div>
+                     <div class="col-sm-6 col-md-7 col-lg-5 wow fadeInRight" data-wow-delay=".1s">
+                        <h5 class="footer-modern-title">Quick Links</h5>
+                        <ul class="footer-modern-list footer-modern-list-2 d-sm-inline-block d-md-block">
+                           <li><a href="#">About Us</a></li>
+                           <li><a href="">Our Team</a></li>
+                           <li><a href="">Shop</a></li>
+                           <li><a href="#">Pricing</a></li>
+                           <li><a href="#">FAQ</a></li>
+                           <li><a href="">Contact Us</a></li>
+                           <li><a href="">What We Offer</a></li>
+                           <li><a href="">Our Blog</a></li>
+                           <li><a href="">Testimonials</a></li>
+                           <li><a href="#">Baking School</a></li>
+                           <li><a href="#">Our Recipes</a></li>
+                        </ul>
+                     </div>
+                     <div class="col-sm-6 col-md-5 col-lg-4 col-xl-3 wow fadeInRight" data-wow-delay=".2s">
+                        <h5 class="footer-modern-title">Get in touch</h5>
+                        <ul class="contacts-creative">
+                           <li>
+                              <div class="unit unit-spacing-sm flex-column flex-md-row">
+                                 <div class="unit-left"><span class="icon mdi mdi-map-marker"></span></div>
+                                 <div class="unit-body"><a href="#">520 Sylvan Ave, 5th Floor<br>Mountain View, CA 94041 USA</a></div>
+                              </div>
+                           </li>
+                           <li>
+                              <div class="unit unit-spacing-sm flex-column flex-md-row">
+                                 <div class="unit-left"><span class="icon mdi mdi-phone"></span></div>
+                                 <div class="unit-body"><a href="tel:#">+1 (540) 615-3333</a></div>
+                              </div>
+                           </li>
+                           <li>
+                              <div class="unit unit-spacing-sm flex-column flex-md-row">
+                                 <div class="unit-left"><span class="icon mdi mdi-email-outline"></span></div>
+                                 <div class="unit-body"><a href="mailto:#">info@mysweetbaker.com</a></div>
+                              </div>
+                           </li>
+                        </ul>
+                        <ul class="list-inline list-social-3 list-inline-sm">
+                           <li><a class="icon mdi mdi-facebook icon-xxs" href="#"></a></li>
+                           <li><a class="icon fa-brands fa-x-twitter icon-xxs" href="#"></a></li>
+                           <li><a class="icon mdi mdi-instagram icon-xxs" href="#"></a></li>
+                           <li><a class="icon mdi mdi-google-plus icon-xxs" href="#"></a></li>
+                           <li><a class="icon mdi mdi-skype icon-xxs" href="#"></a></li>
+                        </ul>
+                     </div>
+                  </div>
+               </div>
+            </div>
+            <div class="footer-modern-panel text-center">
+               <div class="container">
+                  <p class="rights"><span>©&nbsp;</span><span class="copyright-year">2026</span><span>.&nbsp;</span><span>Sweet Baker</span><span>.&nbsp;</span><span>All Rights Reserved</span><span>.&nbsp;</span><a href="">Privacy Policy.</a></p>
+               </div>
+            </div>
+         </footer>
+      </div>
+      <div class="snackbars" id="form-output-global"></div>
+      <script src="./assets/core.min.js.download"></script>
+      <script src="./assets/script.js.download"></script>
+   </body>
+</html>
